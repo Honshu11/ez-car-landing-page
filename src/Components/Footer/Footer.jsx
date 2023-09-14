6e9da7162948eda54f36fabd617b2088736a680f
Honshu11 <egonza67@yahoo.com>
2023-09-14 13:56:47 -0700

    add footer company logo, social media and google maps.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,10 +2,34 @@ import './style.css';
 
 function Footer() {
   return (
-    <div className='FooterContainer'>
-      <i className="bi-alarm" style={{fontSize: "2rem", color: "cornflowerblue"}}></i>
-      <div>This is footer</div>
-    </div>
+    <footer className='footer'>
+      <div className='container'>
+        <div className="row">
+          <div className="col-lg-3 col-md-6">
+            <div className="mb-4">
+              <img src="./Images/ezlogo.png" alt="ez-car-logo" className='img-fluid'/>
+            </div>
+            <div className="social-icons">
+              <a href="" target='_blank'>
+                <i className='bi bi-facebook mr-2'></i>
+              </a>
+              <a href="" target='_blank'>
+                <i className='bi bi-google mr-2'></i>
+              </a>
+              <a href="" target='_blank'>
+                <i className='bi bi-yelp mr-2'></i>
+              </a>
+              <a href="" target='_blank'>
+                <i className='bi bi-instagram mr-2'></i>
+              </a>
+              <a href="" target='_blank'>
+                <i className='bi bi-nextdoor mr-2'></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
