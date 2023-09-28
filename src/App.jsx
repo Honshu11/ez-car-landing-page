6b4438d6e1497972d7d13cdeb8ed0b869e1b7d51
MS07062000 <singhmurali.14@gmail.com>
2023-09-29 00:38:11 +0530

    Services Import had error solved

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './Components/About/About';
 import Appointment from './Components/Appointment/Appointment';
 import ContactUs from './Components/ContactUs/ContactUs';
 import Blog from './Components/Blog/Blog';
-import Services from './Components/Services/services';
+import Services from './Components/Services/Services';
 import './App.css'
 function App() {
   return (
