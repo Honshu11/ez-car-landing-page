dca561c5131ed5e45ba41b52004ce647b9c274ae
Honshu11 <egonza67@yahoo.com>
2023-10-16 12:05:38 -0700

    added fix to about component.

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -19,36 +19,9 @@ function About() {
 
 function About() {
   return (
-    <div id="about" className="container col-xxl-8 text-light">
-<<<<<<< HEAD
-      <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div className="col-10 col-sm-8 col-lg-6">
-          <div id="carousel" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img src=".\Images\ezcar-getYourStickersLicense.png" alt="license plate picture." />
-              </div>
-              <div className="carousel-item active">
-                <img src=".\Images\EzCar-skipTheLine.png" alt="People waiting in line." />
-              </div>
-              <div className='carousel-item active'>
-                <img src=".\Images\ezcar-alldocs.png" alt="information about what ez car registration svcs do." />
-              </div>
-            </div>
-            <button className='carousel-control-prev' type='button' data-bs-target='carousel' data-bs-slide='prev'>
-              <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-              <span className='visually-hidden'>Previous</span>  
-            </button>
-            <button src='carousel-control-next' type='button' data-bs-target='carousel' data-bs-slide='next'>
-              <span className='carousel-control-next-icon' aria-hidden='true'></span>
-              <span className='visually-hidden'>Next</span>
-            </button>
-          </div>
-        </div>
-=======
+    <div>
       <div className="d-flex flex-lg-row flex-column-reverse align-items-center g-5 py-5">
->>>>>>> c621b79327611b4437da5697b20e732336f5f708
-        <div className="col-lg-6">
+        <div className="col-lg-6 text-white">
           <h1 className="display-5 fw-bold 1h-1 mb-3">EZ Car Registration Services</h1>
           <p className="lead"> EZ Car Registration Services is a family-owned company that specializes in Business Partner Automation (BPA) programs.We are authorized to process various vehicle-related transactions, making it convenient for our customers.</p>
         </div>
