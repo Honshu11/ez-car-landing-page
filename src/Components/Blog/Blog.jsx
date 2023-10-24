23e3d235df39c3d1495e16adf12772a535fe1239
MS07062000 <singhmurali.14@gmail.com>
2023-10-24 16:59:38 +0530

    Added google sheets linkage process for blogs

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,11 +2,23 @@ import BlogCard from './BlogCard/BlogCard';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBlog } from '@fortawesome/free-solid-svg-icons';
-import blogs from './blogData';
+import { getBlogs } from './newBlogdata.js';
+import { useState,useEffect } from 'react';
+// import blogs from './blogData';
 function Blog() {
+  const [blogs, setBlogs] = useState([]);
+  async function getBlogData() {
+    const blogs = await getBlogs();
+    setBlogs(blogs);
+  }
+
+  useEffect(()=>{
+    getBlogData();
+  },[])
+
   return (
     <div id='blog' className='container'>
-      <h2 className='blogContainerTitle'><FontAwesomeIcon icon={faBlog} /> EZ Car Blog <FontAwesomeIcon icon={faBlog} /></h2>
+      <h2 className='blogContainerTitle'><FontAwesomeIcon icon={faBlog} /> Blogs <FontAwesomeIcon icon={faBlog} /></h2>
       <div className="blog_Container">
         {blogs.map((blogInfo, index) =>
           <BlogCard key={index} blogInfo={blogInfo} />
--- src/Components/Blog/BlogCard/BlogCard.jsx
+++ src/Components/Blog/BlogCard/BlogCard.jsx
@@ -13,21 +13,19 @@ function BlogCard({ blogInfo }) {
     return (
         <div className="blog_card" onClick={() => redirectToBlogLink(blogInfo.blogLink)}>
             <div className="blog_card_header">
-                {/*Replace src value with blogInfo.blogImage*/}
-                <img src="https://source.unsplash.com/600x400/?computer" alt="blogImage" className="blog_img card_image" />
+                <img src={blogInfo.BlogImage} alt="blogImage" className="blog_img card_image" />
             </div>
             <div className="blog_card_body">
-                <span className={`blog_tag ${blogInfo.tagColor}`}>{blogInfo.tag}</span>
-                <h4>{blogInfo.title}</h4>
-                <p>{blogInfo.description}</p>
+                <span className={`blog_tag ${blogInfo.TagColor}`}>{blogInfo.Tag}</span>
+                <h4>{blogInfo.Title}</h4>
+                <p>{blogInfo.Description}</p>
             </div>
             <div className="blog_card_footer">
                 <div className="blog_user">
-                    {/*Replace src value with blogInfo.authorImage*/}
-                    <img src="https://i.pravatar.cc/40?img=1" alt="authorImage" className="blog_img blog_user_image" />
+                    <img src={blogInfo.AuthorImage} alt="authorImage" className="blog_user_image" />
                     <div className="blog_user_info">
-                        <h5>{blogInfo.author}</h5>
-                        <small>{blogInfo.time}</small>
+                        <h5>{blogInfo.Author}</h5>
+                        <small>{blogInfo.CreationDateTime}</small>
                     </div>
                 </div>
             </div >
@@ -35,4 +33,4 @@ function BlogCard({ blogInfo }) {
     )
 }
 
-export default BlogCard
+export default BlogCard;
